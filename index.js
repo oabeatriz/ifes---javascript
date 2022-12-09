27e4abe02229ea525fa031739a62a0a35a8548c9
oabeatriz <>
2022-12-09 19:44:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// operadores aritméticos
+x = 53
+y = 24
+
+soma = x + y
+subtracao = x - y
+multiplicacao = x * y 
+quociente = x / y 
+resto = x % y 
+potencia = x ** y 
+
+console.log("x =", x, " y =", y)
+console.log("Soma:", soma)
+console.log("Subtração:", subtracao)
+console.log("Multiplicação:", multiplicacao)
+console.log("Quociente:", quociente)
+console.log("Resto:", resto)
+console.log("Potência:", potencia)
